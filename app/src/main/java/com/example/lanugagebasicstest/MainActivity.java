089d7570e4c9e8255234e92b06f50bb16545e83d
Rahul <rahulbodh819@gmail.com>
2024-12-12 14:40:32 +0530

    time differnces in second , milliseconds and minutes

--- app/src/main/java/com/example/lanugagebasicstest/MainActivity.java
+++ app/src/main/java/com/example/lanugagebasicstest/MainActivity.java
@@ -20,5 +20,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        long startTimer = System.currentTimeMillis();
+        System.out.println("startTimer: " + startTimer + " milliseconds");
+
+        try {
+            Thread.sleep(2000); // Wait for 2 seconds
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        long endTimer = System.currentTimeMillis();
+        System.out.println("endTimer: " + endTimer + " milliseconds");
+
+        long difference = endTimer - startTimer;
+
+        long differenceInSeconds = difference / 1000;
+        long differenceInMinutes = difference / (1000 * 60);
+
+        System.out.println("Difference in milliseconds: " + difference);
+        System.out.println("Difference in seconds: " + differenceInSeconds);
+        System.out.println("Difference in minutes: " + differenceInMinutes);
+
     }
 }
